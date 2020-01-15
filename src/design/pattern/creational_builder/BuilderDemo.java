e71afd47c72744829e382f799e3872984ca5656e
dpalankar <deepakpalankar09@gmail.com>
2020-01-16 01:20:19 +0530

    Added Builder design pattern.

---
+++ src/design/pattern/creational_builder/BuilderDemo.java
@@ -0,0 +1,63 @@
+package design.pattern.creational_builder;
+
+final class Student {
+
+	private final int id;
+	private final String name;
+	private final String address;
+
+	public Student(Builder builder) {
+		this.id = builder.id;
+		this.name = builder.name;
+		this.address = builder.address;
+	}
+
+	// Static class Builder
+	public static class Builder {
+
+		/// instance fields
+		private int id;
+		private String name;
+		private String address;
+
+		public static Builder newInstance() {
+			return new Builder();
+		}
+
+		private Builder() {
+		}
+
+		public Builder setId(int id) {
+			this.id = id;
+			return this;
+		}
+
+		public Builder setName(String name) {
+			this.name = name;
+			return this;
+		}
+
+		public Builder setAddress(String address) {
+			this.address = address;
+			return this;
+		}
+
+		// build method to deal with outer class
+		// to return outer instance
+		public Student build() {
+			return new Student(this);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "id = " + this.id + ", name = " + this.name + ", address = " + this.address;
+	}
+}
+
+public class BuilderDemo {
+	public static void main(String args[]) {
+		Student student = Student.Builder.newInstance().setId(1).setName("Ram").setAddress("Noida").build();
+		System.out.println(student);
+	}
+}
---
+++ src/design/pattern/creational_builder/README.md
@@ -0,0 +1,3 @@
+* Builder pattern builds a complex object using simple objects and using a step by step approach.
+* Builder pattern aims to “Separate the construction of a complex object from its representation so that the same construction process can create different representations.”
+* Builder pattern solves the issue with large number of optional parameters and inconsistent state by providing a way to build the object step-by-step and provide a method that will actually return the final Object.
