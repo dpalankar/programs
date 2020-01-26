08278c9b0b519047099016cc561843989e9324a9
dpalankar <deepakpalankar09@gmail.com>
2020-01-27 00:07:50 +0530

    Added proxy design pattern.

---
+++ src/design/pattern/structural_proxy/OfficeInternetAccess.java
@@ -0,0 +1,5 @@
+package design.pattern.structural_proxy;
+
+public interface OfficeInternetAccess {
+	public void grantInternetAccess();
+}
---
+++ src/design/pattern/structural_proxy/ProxyPatternClient.java
@@ -0,0 +1,8 @@
+package design.pattern.structural_proxy;
+
+public class ProxyPatternClient {
+	public static void main(String[] args) {
+		OfficeInternetAccess access = new ProxyInternetAccess("Ashwani Rajput");
+		access.grantInternetAccess();
+	}
+}
