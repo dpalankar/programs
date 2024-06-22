28f6029f8900216bc8061a3152377d7856bc5f5c
Deepak Palankar <deepakpalankar09@gmail.com>
2024-06-22 23:28:23 +0530

    Now project is compatiable with intellej IDE

--- src/example/tricky/FinalTest.java
+++ src/example/tricky/FinalTest.java
@@ -5,7 +5,7 @@ public class FinalTest {
 	public static void main(String[] args) {
 		final int count=5;
 		final String name="Ragya";
-		count =6;
+//		count =6;
 		System.out.println(count);
 	}
 
--- src/example/tricky/Test2.java
+++ src/example/tricky/Test2.java
@@ -1,12 +1,12 @@
 package example.tricky;
 public class Test2 {
 
-	public static String toString(){
-		System.out.println("Test toString called");
-		return "";
-	}
+//	public static String toString(){
+//		System.out.println("Test toString called");
+//		return "";
+//	}
 	
-	public static void main(String args[]){
-		System.out.println(toString());
-	}
+//	public static void main(String args[]){
+//		System.out.println(toString());
+//	}
 }
